6a51e6367f931148a06f977d673e48a7ad59358b
aksha <aksha@DESKTOP-UGPTUDF>
2026-01-11 20:31:26 +0530

    initial commit - spring boot project

---
+++ src/main/java/com/demo/springboot_ci_cd_demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo.springboot_ci_cd_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Ajju - Spring Boot CI/CD Demo";
+    }
+}
